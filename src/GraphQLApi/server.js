61476757971295fbf9e1843dc9028e9fe24401d5
Hugo Girard <64166891+hugogirard@users.noreply.github.com>
2022-07-25 13:08:15 +0000

    added fix

--- src/GraphQLApi/server.js
+++ src/GraphQLApi/server.js
@@ -4,22 +4,22 @@ const port = process.env.PORT || 4000;
 
 const typeDefs = gql`
 type Query {
-  sessions: [Session]
+  sessions:[Session]
 }
 type Session {
-  id: ID!,
+  id: ID!
   title: String!,
-  description: String,
-  startsAt: String,
-  endsAt: String,
-  room: String,
-  day: String,
+  description:String,
+  startsAt:String,
+  endsAt:String,
+  room:String,
+  day:String,
   format: String,
-  track: String,
-  level: String
-}`;
+  track:String,
+  level:String
+}`
 
-const server = new ApolloServer({typeDefs});
+const server = new ApolloServer({ typeDefs });
 
 server.listen(port)
       .then(({ url }) => {
